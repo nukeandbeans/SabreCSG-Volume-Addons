f5f83ead7cc07b60b2cd5bac2188ec575d06dc3d
Daniel Cornelius <dcorn94@gmail.com>
2018-10-28 01:20:45 -0500

    Add Reflection Probe Volume
    + Added classes and materials for reflection probe volumes
    
    NOTE:
    This cannot be used to place reflection probes in its current state. It is not implemented, as this is only a skeleton set of classes.

---
+++ ReflectionProbeVolume/ReflectionProbeVolume.cs
@@ -0,0 +1,96 @@
+﻿#if UNITY_EDITOR || RUNTIME_CSG
+
+using System;
+using System.Linq;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+#endif
+
+namespace Sabresaurus.SabreCSG.Volumes
+{
+	public class ReflectionProbeVolume : Volume
+	{
+		public override Material BrushPreviewMaterial
+		{
+			get
+			{
+				return LoadMaterial( "Data/scsg_volume_reflection.mat", "ReflectionProbeVolume" );//(Material)SabreCSGResources.LoadObject( "Resources/Materials/scsg_volume_reverb.mat" );
+			}
+		}
+
+#if UNITY_EDITOR
+
+
+		public override bool OnInspectorGUI( Volume[] selectedVolumes )
+		{
+			var reverbVolumes = selectedVolumes.Cast<ReflectionProbeVolume>();
+			bool invalidate = false;
+			GUILayout.BeginVertical( "Box" );
+			{
+				GUILayout.Label( "Volume", EditorStyles.boldLabel );
+
+				EditorGUI.indentLevel = 1;
+
+				EditorGUI.indentLevel = 0;
+			}
+			GUILayout.EndVertical();
+			GUILayout.BeginVertical( "Box" );
+			{
+				GUILayout.Label( "Reflection Probe", EditorStyles.boldLabel );
+
+				EditorGUI.indentLevel = 1;
+
+				EditorGUI.indentLevel = 0;
+			}
+			GUILayout.EndVertical();
+
+			return invalidate;
+		}
+
+		private Material LoadMaterial( string path, string relativeToScriptName )
+		{
+			Material loadedObject = (Material)AssetDatabase.LoadMainAssetAtPath( Path.Combine( GetLocalResourcePath(relativeToScriptName), path ) );
+			
+			if( loadedObject != null )
+			{
+				return loadedObject;
+			}
+			return new Material( Shader.Find( "Unlit/Colored" ) );
+		}
+
+
+		private string GetLocalResourcePath(string scriptName)
+		{
+			string[] guids = AssetDatabase.FindAssets( scriptName + " t:Script" );
+
+			foreach( string guid in guids )
+			{
+				string path = AssetDatabase.GUIDToAssetPath( guid );
+				string suffix = scriptName + ".cs";
+
+				//Debug.Log( path );
+
+				if( path.EndsWith( suffix ) )
+				{
+					path = path.Remove( path.Length - suffix.Length, suffix.Length );
+					return path;
+				}
+			}
+			return string.Empty;
+		}
+
+#endif
+		public override void OnCreateVolume( GameObject volume )
+		{
+			ReflectionProbeVolumeComponent rpc = volume.AddComponent<ReflectionProbeVolumeComponent>();
+		}
+}
+}
+
+#endif
---
+++ ReflectionProbeVolume/ReflectionProbeVolumeComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sabresaurus.SabreCSG.Volumes
+{
+	public class ReflectionProbeVolumeComponent : MonoBehaviour
+	{
+		private void Awake()
+		{
+		}
+	}
+}
