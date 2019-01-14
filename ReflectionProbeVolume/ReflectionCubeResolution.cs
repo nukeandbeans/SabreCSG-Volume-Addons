e5b3f8a0c3c9160402a76cfa006536369b59b92e
Daniel Cornelius <dcorn94@gmail.com>
2019-01-14 03:02:22 -0600

    Complete Implementation of reflection probe volume
    Note that due to internal unity issues:
    Setting Occlusion cannot be set from script
    Setting "Custom" settings for probes cannot be set from script.

---
+++ ReflectionProbeVolume/ReflectionCubeResolution.cs
@@ -0,0 +1,14 @@
+﻿namespace Sabresaurus.SabreCSG.Volumes
+{
+    public enum ReflectionCubeResolution : int
+    {
+        _16 = 16,
+        _32 = 32,
+        _64 = 64,
+        _128 = 128,
+        _256 = 256,
+        _512 = 512,
+        _1024 = 1024,
+        _2048 = 2048
+    }
+}
--- ReflectionProbeVolume/ReflectionProbeVolume.cs
+++ ReflectionProbeVolume/ReflectionProbeVolume.cs
@@ -1,10 +1,9 @@
 ﻿#if UNITY_EDITOR || RUNTIME_CSG
 
-using System;
 using System.Linq;
 using UnityEngine;
 using System.IO;
-using System.Collections.Generic;
+using UnityEngine.Rendering;
 
 #if UNITY_EDITOR
 
@@ -16,38 +15,263 @@ namespace Sabresaurus.SabreCSG.Volumes
 {
 	public class ReflectionProbeVolume : Volume
 	{
-		public override Material BrushPreviewMaterial
-		{
-			get
-			{
-				return LoadMaterial( "Data/scsg_volume_reflection.mat", "ReflectionProbeVolume" );//(Material)SabreCSGResources.LoadObject( "Resources/Materials/scsg_volume_reverb.mat" );
-			}
-		}
+
+        public ReflectionProbeMode type = ReflectionProbeMode.Baked;
+        public ReflectionProbeTimeSlicingMode timeMode = ReflectionProbeTimeSlicingMode.AllFacesAtOnce;
+        public ReflectionProbeRefreshMode refreshMode = ReflectionProbeRefreshMode.OnAwake;
+        public int importance = 1;
+        public float intensity = 1;
+        public bool boxProjection = false;
+        public float blendDistance = 1;
+        public ReflectionCubeResolution resolution = ReflectionCubeResolution._128;
+        public bool hdr = true;
+        public float shadowDist = 100;
+        public ReflectionProbeClearFlags clearFlags = ReflectionProbeClearFlags.Skybox;
+        public Color background = new Color32( 49, 77, 128, 0 );
+        public LayerMask cullingMask = ~0;
+        public Vector2 clippingPlanes = new Vector2( 0.3f, 1000.0f ); //x = near, y = far
+        //public bool dynamicObjects = false;
+        //public Cubemap customCube = null;
+
+        private ReflectionProbe p;
 
 #if UNITY_EDITOR
 
-
-		public override bool OnInspectorGUI( Volume[] selectedVolumes )
+        public override Material BrushPreviewMaterial
 		{
-			var reverbVolumes = selectedVolumes.Cast<ReflectionProbeVolume>();
+			get
+			{
+                return LoadMaterial( "Data/scsg_volume_reflection.mat", "ReflectionProbeVolume" );
+            }
+		}
+
+        public override bool OnInspectorGUI( Volume[] selectedVolumes )
+		{
+			var rpVolumes = selectedVolumes.Cast<ReflectionProbeVolume>();
 			bool invalidate = false;
-			GUILayout.BeginVertical( "Box" );
-			{
-				GUILayout.Label( "Volume", EditorStyles.boldLabel );
 
-				EditorGUI.indentLevel = 1;
+			GUILayout.Label( "Reflection Probe", "OL Title" );
 
-				EditorGUI.indentLevel = 0;
-			}
-			GUILayout.EndVertical();
-			GUILayout.BeginVertical( "Box" );
-			{
-				GUILayout.Label( "Reflection Probe", EditorStyles.boldLabel );
+			GUILayout.BeginVertical();
+            {
+                EditorGUI.indentLevel = 1;
 
-				EditorGUI.indentLevel = 1;
+                GUILayout.BeginVertical( "grey_border" );
+                {
+                    GUILayout.Space( 4 );
 
-				EditorGUI.indentLevel = 0;
-			}
+                    EditorGUILayout.LabelField( "General Settings", EditorStyles.boldLabel );
+                    ReflectionProbeMode oldType;
+                    type = (ReflectionProbeMode)EditorGUILayout.EnumPopup( "Type", oldType = type );
+                    if( oldType != type )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.type = type;
+
+                        invalidate = true;
+                    }
+
+                    if( type == ReflectionProbeMode.Custom )
+                    {
+                        EditorGUILayout.HelpBox( "Editing custom probes is currently not supported by volumes. This can be set manually on the volume component.", MessageType.Info );
+                
+                    /*
+                        bool oldDynamicObjects;
+                        dynamicObjects = EditorGUILayout.Toggle( new GUIContent( "Dynamic Objects" ), oldDynamicObjects = dynamicObjects );
+                        if( oldDynamicObjects != dynamicObjects )
+                        {
+                            foreach( ReflectionProbeVolume volume in rpVolumes )
+                                volume.dynamicObjects = dynamicObjects;
+
+                            invalidate = true;
+                        }
+
+                        Cubemap oldCustomCube;
+                        customCube = (Cubemap)EditorGUILayout.ObjectField( "Custom Cube", oldCustomCube = customCube, typeof( Cubemap ), false );
+                        if( oldCustomCube != customCube )
+                        {
+                            foreach( ReflectionProbeVolume volume in rpVolumes )
+                                volume.customCube = customCube;
+
+                            invalidate = true;
+                        }
+                    */
+                    }
+
+                    if( type == ReflectionProbeMode.Realtime )
+                    {
+                        EditorGUI.indentLevel = 2;
+                        ReflectionProbeRefreshMode oldRefreshMode;
+                        refreshMode = (ReflectionProbeRefreshMode)EditorGUILayout.EnumPopup( "Refresh Mode", oldRefreshMode = refreshMode );
+                        if( oldRefreshMode != refreshMode )
+                        {
+                            foreach( ReflectionProbeVolume volume in rpVolumes )
+                                volume.refreshMode = refreshMode;
+
+                            invalidate = true;
+                        }
+
+                        ReflectionProbeTimeSlicingMode oldTimeMode;
+                        timeMode = (ReflectionProbeTimeSlicingMode)EditorGUILayout.EnumPopup( "Time Slicing", oldTimeMode = timeMode );
+                        if( oldTimeMode != timeMode )
+                        {
+                            foreach( ReflectionProbeVolume volume in rpVolumes )
+                                volume.timeMode = timeMode;
+
+                            invalidate = true;
+                        }
+                        EditorGUI.indentLevel = 1;
+                    }
+
+
+                    GUILayout.Space( 4 );
+                }
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical( "grey_border" );
+                {
+                    GUILayout.Space( 4 );
+                    EditorGUILayout.LabelField( "Runtime Settings", EditorStyles.boldLabel );
+
+                    int oldImportance;
+                    importance = EditorGUILayout.IntField( "Importance", oldImportance = importance );
+                    if( oldImportance != importance )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.importance = importance;
+
+                        invalidate = true;
+                    }
+
+                    float oldIntensity;
+                    intensity = EditorGUILayout.FloatField( "Intensity", oldIntensity = intensity );
+                    if( oldIntensity != intensity )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.intensity = intensity;
+
+                        invalidate = true;
+                    }
+
+                    bool oldBoxProj;
+                    boxProjection = EditorGUILayout.Toggle( "Box Projection", oldBoxProj = boxProjection );
+                    if( oldBoxProj != boxProjection )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.boxProjection = boxProjection;
+
+                        invalidate = true;
+                    }
+
+                    float oldBlendDist;
+                    blendDistance = EditorGUILayout.FloatField( "Blend Distance", oldBlendDist = blendDistance );
+                    if( oldBlendDist != blendDistance )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.blendDistance = blendDistance;
+
+                        invalidate = true;
+                    }
+
+                    EditorGUILayout.HelpBox( "Size and origin is set by the volume", MessageType.Info );
+
+                    GUILayout.Space( 4 );
+                }
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical( "grey_border" );
+                {
+                    GUILayout.Space( 4 );
+                    EditorGUILayout.LabelField( "Cubemap Capture Settings", EditorStyles.boldLabel );
+
+                    ReflectionCubeResolution oldRes;
+                    resolution = (ReflectionCubeResolution)EditorGUILayout.EnumPopup( "Resolution", oldRes = resolution );
+                    if( oldRes != resolution )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.resolution = resolution;
+
+                        invalidate = true;
+                    }
+
+                    bool oldHDR;
+                    hdr = EditorGUILayout.Toggle( "HDR", oldHDR = hdr );
+                    if( oldHDR != hdr )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.hdr = hdr;
+
+                        invalidate = true;
+                    }
+
+                    float oldShadowDist;
+                    shadowDist = EditorGUILayout.FloatField( "Shadow Distance", oldShadowDist = shadowDist );
+                    if( oldShadowDist != blendDistance )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.shadowDist = shadowDist;
+
+                        invalidate = true;
+                    }
+
+                    ReflectionProbeClearFlags oldCFlags;
+                    clearFlags = (ReflectionProbeClearFlags)EditorGUILayout.EnumPopup( "Clear Flags", oldCFlags = clearFlags );
+                    if( oldCFlags != clearFlags )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.clearFlags = clearFlags;
+
+                        invalidate = true;
+                    }
+
+                    Color oldColor;
+                    background = EditorGUILayout.ColorField( "Background", oldColor = background );
+                    if( oldColor != background )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.background = background;
+
+                        invalidate = true;
+                    }
+
+                    LayerMask oldMask;
+                    cullingMask = SabreGUILayout.LayerMaskField( new GUIContent( "Culling Mask" ), oldMask = cullingMask );//EditorGUILayout.MaskField( "Culling Mask", oldMask = cullingMask );
+                    if( oldMask != cullingMask )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.cullingMask = cullingMask;
+
+                        invalidate = true;
+                    }
+
+                    Vector2 oldClip;
+                    clippingPlanes = EditorGUILayout.Vector2Field(new GUIContent("Clipping Planes", "X = Near, Y = Far"), oldClip = clippingPlanes );
+                    if( oldClip != clippingPlanes )
+                    {
+                        foreach( ReflectionProbeVolume volume in rpVolumes )
+                            volume.clippingPlanes = clippingPlanes;
+
+                        invalidate = true;
+                    }
+
+                    EditorGUILayout.HelpBox( "Occlusion is currently not supported by volumes. This can be set on the volume component.", MessageType.Info );
+
+                    GUILayout.Space( 4 );
+                }
+                GUILayout.EndVertical();
+                
+                if( type == ReflectionProbeMode.Custom )
+                {
+                    EditorGUILayout.HelpBox( "This probe is set to Custom, and is not handled by volumes.", MessageType.Info );
+
+                }
+                else if (type == ReflectionProbeMode.Realtime )
+                {
+                    EditorGUILayout.HelpBox( "This probe is set to Realtime, and its baking is handled by unity. The results are stored in the GI Cache.", MessageType.Info );
+                }
+
+                EditorGUI.indentLevel = 0;
+            }
 			GUILayout.EndVertical();
 
 			return invalidate;
@@ -88,9 +312,31 @@ namespace Sabresaurus.SabreCSG.Volumes
 #endif
 		public override void OnCreateVolume( GameObject volume )
 		{
-			ReflectionProbeVolumeComponent rpc = volume.AddComponent<ReflectionProbeVolumeComponent>();
+			ReflectionProbeVolumeComponent rpc = volume.AddOrGetComponent<ReflectionProbeVolumeComponent>();
+            p = volume.GetComponent<ReflectionProbe>();
+            
+            rpc.Setup(); // init bounds and set them up on the probe
+
+            rpc.type = type;
+            rpc.timeMode = timeMode;
+            rpc.refreshMode = refreshMode;
+            rpc.importance = importance;
+            rpc.intensity = intensity;
+            rpc.boxProjection = boxProjection;
+            rpc.blendDistance = blendDistance;
+            rpc.resolution = resolution;
+            rpc.hdr = hdr;
+            rpc.shadowDist = shadowDist;
+            rpc.clearFlags = clearFlags;
+            rpc.background = background;
+            rpc.cullingMask = cullingMask;
+            rpc.clippingPlanes = clippingPlanes;
+            //rpc.dynamicObjects = dynamicObjects;
+            //rpc.customCube = customCube;
+
+            rpc.Apply(); // make sure the probe knows the new settings.
 		}
-}
+    }
 }
 
 #endif
